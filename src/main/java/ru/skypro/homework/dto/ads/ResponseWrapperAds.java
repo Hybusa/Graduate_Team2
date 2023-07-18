665726e6e59ccd3af25aecc8932a6ec3516c54e8
Hybusa <alkouzne007@gmail.com>
2023-07-18 18:20:58 +0300

    Unnecessary stuff removed

--- src/main/java/ru/skypro/homework/dto/ads/ResponseWrapperAds.java
+++ src/main/java/ru/skypro/homework/dto/ads/ResponseWrapperAds.java
@@ -1,11 +1,6 @@
 package ru.skypro.homework.dto.ads;
 
 import lombok.Data;
-import ru.skypro.homework.mapper.AdsMapper;
-import ru.skypro.homework.model.Ad;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 @Data
 public class ResponseWrapperAds {
@@ -15,13 +10,4 @@ public class ResponseWrapperAds {
     public ResponseWrapperAds() {
     }
 
-    public ResponseWrapperAds(List<Ad> ads){
-        this.count = ads.size();
-        List<ResponseAd> responseAds= new ArrayList<>();
-        for (Ad ad : ads) {
-            responseAds.add(AdsMapper.adToResponseAd(ad));
-        }
-        Collections.shuffle(responseAds);
-        this.results = responseAds;
-    }
 }
